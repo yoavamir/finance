0c62272013d5d07341e61acd0b1b3fc02df8e6c7
yoav <yoavamir@gmail.com>
2020-04-23 14:17:48 +0300

    dir

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import fileActions from "../apis/fileActions";
+import dataFetcherBase from "../apis/dataFetcherBase";
 import history from "../history";
 import {
   INIT_DATA,
@@ -17,55 +17,55 @@ import {
 } from "./types";
 
 export const initData = () => async (dispatch) => {
-  const response = await fileActions.post("/init_data");
+  const response = await dataFetcherBase.post("/init_data");
   dispatch({ type: INIT_DATA, payload: response.data });
   history.push("/");
 };
 
 export const uploadFile = (formData) => async (dispatch) => {
-  const response = await fileActions.post("/upload_file", formData);
+  const response = await dataFetcherBase.post("/upload_file", formData);
   dispatch({ type: UPLOAD_FILE, payload: response.data });
   history.push("/");
 };
 
 export const getTotalAmount = () => async (dispatch) => {
-  const response = await fileActions.get("/total_amount");
+  const response = await dataFetcherBase.get("/total_amount");
   dispatch({ type: TOTAL_AMOUNT, payload: response.data });
 };
 
 export const getSpentByDay = (filename) => async (dispatch) => {
-  const response = await fileActions.get("/spent_by_day", {
+  const response = await dataFetcherBase.get("/spent_by_day", {
     params: { filename },
   });
   dispatch({ type: SPENT_BY_DAY, payload: response.data });
 };
 
 export const getShopsDistribution = () => async (dispatch) => {
-  const response = await fileActions.get("/shops_distribution");
+  const response = await dataFetcherBase.get("/shops_distribution");
   dispatch({ type: SHOPS_DISTRIBUTION, payload: response.data });
 };
 
 export const getCategoryDistribution = (filename) => async (dispatch) => {
-  const response = await fileActions.get("/category_distribution", {
+  const response = await dataFetcherBase.get("/category_distribution", {
     params: { filename },
   });
   dispatch({ type: CATEGORY_DISTRIBUTION, payload: response.data });
 };
 
 export const getFileTimeRange = (filename) => async (dispatch) => {
-  const response = await fileActions.get("/time_range", {
+  const response = await dataFetcherBase.get("/time_range", {
     params: { filename },
   });
   dispatch({ type: TIME_RANGE, payload: response.data });
 };
 
 export const getMonthlyExpense = () => async (dispatch) => {
-  const response = await fileActions.get("/spent_by_month");
+  const response = await dataFetcherBase.get("/spent_by_month");
   dispatch({ type: MONTHLY_EXPENSE, payload: response.data });
 };
 
 export const getShopExpenseByMonth = () => async (dispatch) => {
-  const response = await fileActions.get("/shops_by_months");
+  const response = await dataFetcherBase.get("/shops_by_months");
   dispatch({ type: SHOPS_BY_MONTHS, payload: response.data });
 };
 
