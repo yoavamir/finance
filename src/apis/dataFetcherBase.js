5ae63d304f38ce5612177c70fd8485dc17485e7c
yoav <yoavamir@gmail.com>
2020-04-30 19:01:59 +0300

    asd

--- src/apis/dataFetcherBase.js
+++ src/apis/dataFetcherBase.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://finance-server-yoav-daphna.herokuapp.com",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://finance-server-yoav-daphna.herokuapp.com",
+  // baseURL: "http://localhost:5000/",
 });
