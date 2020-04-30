f46e78d684c88df73b1514fdb534102bbaea4dec
yoav <yoavamir@gmail.com>
2020-04-30 18:55:44 +0300

    style

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import FetchDataButton from "../components/FetchDataButton";
 import StartProcessButton from "../components/StartProcessButton";
-import { Header, Grid, Container } from "semantic-ui-react";
+import { Header, Grid, Container, Responsive } from "semantic-ui-react";
+
+const COLUMN_WIDTH = 4;
 
 const WelcomePage = ({ dataFetched }) => {
   const renderText = () => {
@@ -14,40 +16,51 @@ const WelcomePage = ({ dataFetched }) => {
             Welcome to Yoav and Daphna finance page
           </Header>
           <Grid textAlign="center">
-            <Grid.Row>
-              <Grid.Column width={6}>
-                <StartProcessButton></StartProcessButton>
-              </Grid.Column>
-              <Grid.Column width={6}>
-                <FetchDataButton message="Start with mock data!"></FetchDataButton>
-              </Grid.Column>
-            </Grid.Row>
+            <Grid.Column width={6}>
+              <StartProcessButton></StartProcessButton>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <FetchDataButton message="Start with mock data!"></FetchDataButton>
+            </Grid.Column>
           </Grid>
         </Container>
       );
     } else {
       return (
-        <div style={{ margin: "20px" }}>
+        <Container>
           <Header as="h2" textAlign="center">
             <Header.Content>
               Click any of the buttons to show data about you money!
             </Header.Content>
           </Header>
-          <div className="ui grid" textalign="center">
-            <Link to="/shops_distribution" className="ui button primary">
-              Show shops distribution
-            </Link>
-            <Link to="/spent_by_month" className="ui button primary">
-              Show Montly expenses
-            </Link>
-            <Link to="/shops_expenses_by_month" className="ui button primary">
-              Show shops by months
-            </Link>
-            <Link to="/monthly_balance" className="ui button primary">
-              Show monthly balance
-            </Link>
-          </div>
-        </div>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={COLUMN_WIDTH}>
+                <Link to="/shops_distribution" className="ui button primary">
+                  Show shops distribution
+                </Link>
+              </Grid.Column>
+              <Grid.Column width={COLUMN_WIDTH}>
+                <Link to="/spent_by_month" className="ui button primary">
+                  Show Montly expenses
+                </Link>
+              </Grid.Column>
+              <Grid.Column width={COLUMN_WIDTH}>
+                <Link
+                  to="/shops_expenses_by_month"
+                  className="ui button primary"
+                >
+                  Show shops by months
+                </Link>
+              </Grid.Column>
+              <Grid.Column width={COLUMN_WIDTH}>
+                <Link to="/monthly_balance" className="ui button primary">
+                  Show monthly balance
+                </Link>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       );
     }
   };
